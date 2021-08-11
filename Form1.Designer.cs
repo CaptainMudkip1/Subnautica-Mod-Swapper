b877805247ec418b9f99a1889fee9903814b08a8
Broken-OS <73603426+Broken-OS@users.noreply.github.com>
2021-08-11 09:21:04 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace Subnautica_Mod_Swapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.yes_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // yes_button
+            // 
+            this.yes_button.Location = new System.Drawing.Point(713, 415);
+            this.yes_button.Name = "yes_button";
+            this.yes_button.Size = new System.Drawing.Size(75, 23);
+            this.yes_button.TabIndex = 0;
+            this.yes_button.Text = "button1";
+            this.yes_button.UseVisualStyleBackColor = true;
+            this.yes_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.yes_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button yes_button;
+    }
+}
+
